8edf0c0f5d2e7ad89311bd6ba0e29dbee5e24c9b
Christian Schladetsch <christian.schladetsch@gmail.com>
2018-07-05 20:41:36 +1000

    Using OpenCV to resize images before processing

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Flurl.Http;
 using WebEye.Controls.WinForms.WebCameraControl;
 
 using Newtonsoft.Json;
+using OpenCvSharp;
 
 // Cards are: 63 x 88 mm
 // 63/88 = 1.4 aspect ratio
@@ -64,14 +64,6 @@ namespace WindowsFormsApp1
             public List<Response> responses;
         }
 
-        private async Task AnalizeFileAsync(string fileName)
-        {
-            var bytes = System.IO.File.ReadAllBytes(fileName);
-            var result = await PostAsync(System.Convert.ToBase64String(bytes));
-            var res = JsonConvert.DeserializeObject<VisionResponse>(result);
-            ProcessResponse(res);
-        }
-
         private void ProcessResponse(VisionResponse res)
         {
             if (res == null)
@@ -99,10 +91,34 @@ namespace WindowsFormsApp1
             return title.TrimEnd('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' ');
         }
 
-        async Task<string> PostAsync(string base64Content)
+        private void openImageToolStripMenuItem_ClickAsync(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result != DialogResult.OK)
+                return;
+
+            ProcessFile(openFileDialog1.FileName);
+        }
+
+        private async void ProcessFile(string fileName)
+        {
+            var src = new OpenCvSharp.Mat(fileName, ImreadModes.GrayScale);
+            var width = (float)src.Width;
+            var height = (float)src.Height;
+            var aspect = width / height;
+            width = 200;
+            height = width/aspect;
+            var dest = src.Resize(new Size(width, height));
+            var bytes = dest.ToBytes(ext: ".jpg");
+            var result = await Post(System.Convert.ToBase64String(bytes));
+            var res = JsonConvert.DeserializeObject<VisionResponse>(result);
+            ProcessResponse(res);
+        }
+
+        async Task<string> Post(string base64Content)
         {
             // hacky, but it works
-            var json = @"
+            var text = @"
             {
              ""requests"": [
               {
@@ -120,31 +136,12 @@ namespace WindowsFormsApp1
             ".Replace("$CONTENT", base64Content);
             var baseUrl  = "https://vision.googleapis.com/v1/images:annotate?fields=responses&key=" + _visionApiKey;
 
-            using (var client = new HttpClient())
-            {
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync(baseUrl, content);
-                Console.WriteLine($"Sending request {content.Headers}");
-                return await response.Content.ReadAsStringAsync();
-            }
+            var response = await baseUrl.PostJsonAsync(JsonConvert.DeserializeObject(text));
+            return await response.Content.ReadAsStringAsync();
         }
 
         private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
-        }
-
-        private void openImageToolStripMenuItem_ClickAsync(object sender, EventArgs e)
-        {
-            var result = openFileDialog1.ShowDialog();
-            if (result != DialogResult.OK)
-                return;
-
-            ProcessFileAsync(openFileDialog1.FileName);
-        }
-
-        private async void ProcessFileAsync(string fileName)
-        {
-            await AnalizeFileAsync(fileName);
         }
 
         private void writeToolStripMenuItem_Click(object sender, EventArgs e)
